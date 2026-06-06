f9c6cb9f6f40bf884ad897e4773421ced2cdf3b2
Gustavo Cardoso <gusstavocardoso@gmail.com>
2026-06-05 21:19:19 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -27,6 +27,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 // Inicialização do servidor
-app.listen(4000, () => {
+app.listen(4000, '0.0.0.0', () => {
   console.log('Backend rodando na porta 4000');
 });
+
